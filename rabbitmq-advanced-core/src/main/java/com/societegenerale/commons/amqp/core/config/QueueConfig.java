85620badd32d33835fcf3151054e49cf444296e9
Vincent Fuchs <vincent.fuchs@gefa.de>
2022-08-26 11:03:58 +0200

    no deprecated use of StringUtils

--- rabbitmq-advanced-core/src/main/java/com/societegenerale/commons/amqp/core/config/QueueConfig.java
+++ rabbitmq-advanced-core/src/main/java/com/societegenerale/commons/amqp/core/config/QueueConfig.java
@@ -19,9 +19,9 @@ package com.societegenerale.commons.amqp.core.config;
 import com.societegenerale.commons.amqp.core.exception.RabbitmqConfigurationException;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.amqp.core.Queue;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.util.StringUtils;
 
 import java.util.Map;
 
